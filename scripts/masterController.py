6465b319fb13dc92d9f5b3c6b09d8bbc79cc2310
damelio2@illinois.edu <damelio2@bl10-analysis2.sns.gov>
2026-06-13 14:25:13 -0400

    Implemented the rolling of images and duplication protection

--- scripts/masterController.py
+++ scripts/masterController.py
@@ -1,4 +1,116 @@
+import paneMakers.rolledImagePanes
+from pathlib import Path
+import re
+
+MASTER_IMAGE_SOURCE = Path("/SNS/VENUS/IPTS-36967/shared/autoreduce/images/tpx1/raw/radiography/")
+ENERGY_FRAME_MIN = 560
+ENERGY_FRAME_MAX = 830
+
+
+STARTING_RUN_NUMBER = 23417
+ENDING_RUN_NUMBER = 23535
+RUN_NUMBERS = list(range(STARTING_RUN_NUMBER, ENDING_RUN_NUMBER + 1))
+
+EXPECTED_IMAGE_DURATION = 5 #How long one frame was collected for, not the total length of the rolled frames
+
+MASTER_DESTINATION = Path("/SNS/VENUS/IPTS-36967/shared/Batch_analysis_6-12-25/June/HUDtest/")
+OB_PATH = Path(
+	"/SNS/VENUS/IPTS-36967/shared/Batch_analysis_6-12-25/June/OBs/")
+
+def batch_process_images_into_rolls(master_image_source = MASTER_IMAGE_SOURCE, run_number_range = RUN_NUMBERS, master_file_destination = MASTER_DESTINATION, scanLen_min = EXPECTED_IMAGE_DURATION, ob_path = OB_PATH, roll_length= 5): #will perform a series of data processing steps, and then caclulate plots to make composite image
+
+    #Creation of simple division normalized images, some of which will be summed ("Rolled") for higher contrast
+    #choose roll lengths
+    roll_lengths = [roll_length]
+    for rollLength in roll_lengths:
+
+        window_size = rollLength / scanLen_min
+        if window_size % 1 != 0:
+            print ("Error: Cannot make ", rollLength, " min rolls with ", scanLen_min, " min scans")
+        else:
+            print("Rolling ", window_size, "scans together")
+
+        window_size = int(window_size)
+        #Determine what run numbers already exist in target location
+        targetLocation_for_rolls = Path (master_file_destination / (str(rollLength) + "rolls/"))
+
+        if targetLocation_for_rolls.exists() == False:
+            print("Creating:", targetLocation_for_rolls)
+            targetLocation_for_rolls.mkdir(parents=True, exist_ok=True)
+
+        # ==========================================================
+        # enumerate all .tif files in this folder
+        # ==========================================================
+        tif_files = sorted(targetLocation_for_rolls.glob("*.tif"))
+
+        print(f"Found {len(tif_files)} .tif files in {targetLocation_for_rolls}")
+        pre_existing_images = []
+        # optional: print names for debug
+        for f in tif_files:
+            pre_existing_images.append(f.name)
+        
+
+
+        
+        runs_which_have_not_been_processed_yet = []
+        if len(tif_files) == 0:
+            print("No prexisting data found")
+            runs_which_have_not_been_processed_yet = run_number_range
+        else: #find the most recent run number that has bin processed
+
+            max_run = -float("inf")
+
+            for f in tif_files:
+
+                # extract all numbers in filename
+                numbers = re.findall(r"\d+", f.stem)
+
+                if not numbers:
+                    continue
+
+                # convert to ints
+                numbers = list(map(int, numbers))
+
+                # take max number in this file
+                file_max = max(numbers)
+
+                # update global max
+                if file_max > max_run:
+                    max_run = file_max
+
+            print("Largest run number found:", max_run)
+
+            #compute which preceding frames are need to complete the immediate frames after the max frame
+            runs_which_have_not_been_processed_yet = list(range(max_run-window_size, run_number_range[-1]))
+
+
+
+        #def process_runs_rolling_global(master_image_source, file_destination, run_numbers, open_beam, window_size, image_duration, duration_percentage_filter = 20):
+        newImages = paneMakers.rolledImagePanes.process_runs_rolling_global(master_image_source, master_file_destination / (str(rollLength) + "rolls/"), runs_which_have_not_been_processed_yet, ob_path / ("June_OB_" + str(rollLength) + "min.tif" ),  window_size, scanLen_min*60, energyLabels = [560, 830] , duration_percentage_filter = 20)
+        allImages = pre_existing_images + newImages
+
+        return allImages
 
 
 
 
+
+    DURATION_CHECK_THRESHOLD_PERCENTAGE = 20 # Runs that deviate by more than this percentage 
+
+    NORMALIZE_BOOL = True
+    
+    BAD_FILE_LOG = Path("/SNS/VENUS/IPTS-36967/shared/Batch_analysis_6-12-25/June/Nucleation/quarterPercent/Crystal Images_rolling/badfile.log")
+
+    #FINAL_IMAGE_LENGTH = int(WINDOW_SIZE*EXPECTED_IMAGE_DURATION_SECONDS/60)
+
+    #OB_PATH = Path(
+      #  "/SNS/VENUS/IPTS-36967/shared/Batch_analysis_6-12-25/June/OBs/June_OB_" + str(FINAL_IMAGE_LENGTH) + "min.tif"
+  #  )
+    return 0
+def main():
+    batch_process_images_into_rolls(roll_length = 5)
+    return 0
+
+	
+if __name__ == "__main__":
+	main()
--- scripts/paneMakers/rolledImagePanes.py
+++ scripts/paneMakers/rolledImagePanes.py
@@ -11,30 +11,6 @@ from astropy.io import fits
 # ==========================================================
 
 
-
-
-MASTER_SOURCE = Path("/SNS/VENUS/IPTS-36967/shared/autoreduce/images/tpx1/raw/radiography/")
-FRAME_MIN = 560
-FRAME_MAX = 830
-
-
-A = 23417
-B = 23533
-RUN_NUMBERS = list(range(A, B + 1))
-
-EXPECTED_IMAGE_DURATION_SECONDS = 5 * 60 #How long one frame was collected for, not the total length of the rolled frames
-DURATION_CHECK_THRESHOLD_PERCENTAGE = 20 # Runs that deviate by more than this percentage 
-
-NORMALIZE_BOOL = True
-WINDOW_SIZE = 6
-
-
-BAD_FILE_LOG = Path("/SNS/VENUS/IPTS-36967/shared/Batch_analysis_6-12-25/June/Nucleation/quarterPercent/Crystal Images_rolling/badfile.log")
-FINAL_IMAGE_LENGTH = int(WINDOW_SIZE*EXPECTED_IMAGE_DURATION_SECONDS/60)
-MASTER_DESTINATION = Path("/SNS/VENUS/IPTS-36967/shared/Batch_analysis_6-12-25/June/Nucleation/quarterPercent/Crystal Images_rolling/" + str(FINAL_IMAGE_LENGTH) + "_roll")
-OB_PATH = Path(
-	"/SNS/VENUS/IPTS-36967/shared/Batch_analysis_6-12-25/June/OBs/June_OB_" + str(FINAL_IMAGE_LENGTH) + "min.tif"
-)
 # ==========================================================
 # OUTPUT DIRECTORIES
 # ==========================================================
@@ -49,7 +25,7 @@ OB_PATH = Path(
 # IMAGE LOADER (TIFF + FITS via Astropy)
 # ==========================================================
 
-def load_image(path):
+def load_image(path, badfilelog):
 
 	path = Path(path)
 
@@ -81,7 +57,7 @@ def load_image(path):
 
 	except Exception as e:
 
-		with open(BAD_FILE_LOG, "a") as f:
+		with open( badfilelog, "a") as f:
 			f.write(f"{path} :: {repr(e)}\n")
 
 		print(f"[SKIP] {path} -> {e}")
@@ -107,55 +83,16 @@ def get_HDF_duration(hdf5_input):
 		duration = float(np.array(f["entry/duration"]))
 	return duration
 
-'''
-    with h5py.File(hdf5_input, "r") as f:
-        # Times (convert to Python strings)
-        start_time = np.array(f["entry/start_time"], dtype=str).item()
-        end_time = np.array(f["entry/end_time"], dtype=str).item()
 
-        start_time = clean_iso(start_time)
-        end_time = clean_iso(end_time)
-
-        # Duration
-        duration = float(np.array(f["entry/duration"]))
-
-        # Temperatures
-        start_T = float(np.array(f["entry/DASlogs/BL10:SE:ND2:CH1:PV/minimum_value"]))
-        end_T = float(np.array(f["entry/DASlogs/BL10:SE:ND2:CH1:PV/maximum_value"]))
-        avg_T = float(np.array(f["entry/DASlogs/BL10:SE:ND2:CH1:PV/average_value"]))
-
-        return {
-            "run_number": Run_num,
-            "filename": hdf5_input,
-            "start_time": start_time,
-            "end_time": end_time,
-            "duration": duration,
-            "start_T": start_T,
-            "end_T": end_T,
-            "avg_T": avg_T
-        }
-'''
-# ==========================================================
-# OPTIONAL: FITS → TIFF CONVERSION
-# ==========================================================
-
-def convert_to_tiff(path, image_array):
-
-	out_name = path.stem + ".tif"
-	out_path = CONVERTED_DIR / out_name
-
-	tifffile.imwrite(out_path, image_array.astype(np.float32))
-
-	print(f"[CONVERTED] {path.name} -> {out_path.name}")
 
 
 # ==========================================================
 # OPEN BEAM
 # ==========================================================
 
-def load_open_beam(path):
+def load_open_beam(path,  badfilelog):
 	print("Loading open beam...")
-	return load_image(path)
+	return load_image(path, badfilelog)
 
 
 # ==========================================================
@@ -193,13 +130,13 @@ def collect_images(run_folder, frame_min, frame_max):
 # STACK SUM
 # ==========================================================
 
-def sum_image_stack(files):
+def sum_image_stack(files,  badfilelog):
 
 	summed = None
 
 	for f in files:
 
-		img = load_image(f)
+		img = load_image(f,  badfilelog)
 
 		if img is None:
 			continue
@@ -278,33 +215,37 @@ def discover_all_runs(MASTER_SOURCE, run_min, run_max):
 	return run_map
 
 
-def process_runs_rolling_global(MASTER_SOURCE, run_numbers, open_beam, window_size):
-
-	print(f"\n--- GLOBAL Rolling across ALL containers (N={window_size}) ---")
-	print("Final Image Length:", FINAL_IMAGE_LENGTH)
+def process_runs_rolling_global(master_image_source, file_destination, run_numbers, open_beam, window_size, image_duration, energyLabels = [560, 830] ,duration_percentage_filter = 20):
+	print("Beginning Rolling in range", run_numbers)
+	out_dir = file_destination
+	out_dir.mkdir(parents=True, exist_ok=True)
+	badfilelog = out_dir / "bad_files.log"
+	print(open_beam)
+	open_beam = load_open_beam(open_beam,  badfilelog)
+	energy_min = energyLabels[0]
+	energy_max = energyLabels[1]
 
 	run_min = min(run_numbers)
 	run_max = max(run_numbers)
 
-	run_map = discover_all_runs(MASTER_SOURCE, run_min, run_max)
-	output_path = "postProcess_Run_" +  str(str(run_map[0][1]).split("/")[-1].split("Run_")[1])
+	run_map = discover_all_runs(master_image_source, run_min, run_max)
 	if len(run_map) < window_size:
 		print("Not enough global runs found")
 		return False
 
-	out_dir = MASTER_DESTINATION  / output_path 
-	out_dir.mkdir(parents=True, exist_ok=True)
-	CONVERTED_DIR = MASTER_DESTINATION  / output_path / "converted_tiffs"
-	CONVERTED_DIR.mkdir(parents=True, exist_ok=True)
-
-	BAD_FILE_LOG = MASTER_DESTINATION  / output_path / "bad_files.log"
 	
-	num_windows = len(run_map) - window_size + 1
 
+	
+	
+	num_windows = int(len(run_map) - window_size + 1)
+	#print(window_size)
+	images_made = 0
+	created_images = []
 	for i in range(num_windows):
+		#print(i)
 
 		window_runs = run_map[i:i + window_size]
-
+		#print(window_runs)
 		window_sum = None
 
 		start_run = window_runs[0][0]
@@ -314,20 +255,21 @@ def process_runs_rolling_global(MASTER_SOURCE, run_numbers, open_beam, window_si
 		for run_id, folder, in window_runs:
 			hdf5_data = get_HDF_for_single_run(run_id)
 			duration = get_HDF_duration(hdf5_data)
-			if duration > EXPECTED_IMAGE_DURATION_SECONDS * (1 + (0.01*DURATION_CHECK_THRESHOLD_PERCENTAGE)) or duration < EXPECTED_IMAGE_DURATION_SECONDS * (1 - (0.01*DURATION_CHECK_THRESHOLD_PERCENTAGE)):
+			if duration > image_duration* (1 + (0.01*duration_percentage_filter)) or duration < image_duration * (1 - (0.01*duration_percentage_filter)):
 				rangeContainsBadFrames = True
-				print("Bad Frame:", run_id, duration, "Expected:", EXPECTED_IMAGE_DURATION_SECONDS)
-
+				print("Bad Frame:", run_id, duration, "Expected:", image_duration)
+		
 		#print(window_runs, "aaa")
 		if rangeContainsBadFrames:
 			
 			print("Skip")			
 		else:
+			#print("here")
 			for run_id, folder in window_runs:
 
-				files = collect_images(folder, FRAME_MIN, FRAME_MAX)
+				files = collect_images(folder, energy_min, energy_max)
 				
-				summed = sum_image_stack(files)
+				summed = sum_image_stack(files, badfilelog)
 
 				if summed is None:
 					continue
@@ -339,15 +281,20 @@ def process_runs_rolling_global(MASTER_SOURCE, run_numbers, open_beam, window_si
 
 			if window_sum is None:
 				continue
-
+			#print(type(open_beam))
 			img = normalize_by_open_beam(window_sum, open_beam)
 			img = rotate(img)
 
 			out_path = out_dir / f"GlobalRunWindow_{start_run}_{end_run}.tif"
+			#print(out_path)
 
 			save(img, out_path)
+			created_images.append(out_path)
+			images_made += 1
 
-	return True
+	print("Created ", images_made, " images")
+
+	return created_images
 
 
 # ==========================================================
@@ -365,5 +312,5 @@ def main():
 	print("\nDone.")
 
 
-if __name__ == "__main__":
-	main()
+#if __name__ == "__main__":
+	#main()
