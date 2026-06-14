a5ea97d73d6fce468c1be50f3c4a49e6b016759d
damelio2@illinois.edu <damelio2@analysis-node23.sns.gov>
2026-06-13 21:48:37 -0400

    Abstracted roll making out of controller

--- scripts/paneMakers/rolledImagePanes.py
+++ scripts/paneMakers/rolledImagePanes.py
@@ -4,8 +4,9 @@ import h5py
 import numpy as np
 import tifffile
 from astropy.io import fits
+from PIL import Image
 
-
+import re
 # ==========================================================
 # User Settings
 # ==========================================================
@@ -295,6 +296,143 @@ def process_runs_rolling_global(master_image_source, file_destination, run_numbe
 	print("Created ", images_made, " images")
 
 	return created_images
+
+
+
+
+def batch_process_images_into_rolls(master_image_source, run_number_range, master_file_destination, scanLen_min, ob_path, roll_length): #will perform a series of data processing steps, and then caclulate plots to make composite image
+    def grey_balance(arr, low=1, high=99):
+        """
+        Percentile-based contrast normalization.
+        Works on grayscale or RGB uint16/float images.
+        """
+
+        arr = arr.astype(np.float32)
+
+        # if RGB, operate per-channel
+        if arr.ndim == 3:
+            out = np.zeros_like(arr, dtype=np.float32)
+
+            for c in range(arr.shape[2]):
+                channel = arr[:, :, c]
+
+                lo = np.percentile(channel, low)
+                hi = np.percentile(channel, high)
+
+                if hi - lo < 1e-6:
+                    out[:, :, c] = 0
+                else:
+                    out[:, :, c] = (channel - lo) / (hi - lo)
+
+            arr = out
+        else:
+            lo = np.percentile(arr, low)
+            hi = np.percentile(arr, high)
+
+            if hi - lo > 1e-6:
+                arr = (arr - lo) / (hi - lo)
+            else:
+                arr = np.zeros_like(arr)
+
+        
+        return np.clip(arr, 0, 1)
+    #Creation of simple division normalized images, some of which will be summed ("Rolled") for higher contrast
+    #choose roll lengths
+    roll_lengths = [roll_length]
+    for rollLength in roll_lengths:
+
+        window_size = rollLength / scanLen_min
+        if window_size % 1 != 0:
+            print ("Error: Cannot make ", rollLength, " min rolls with ", scanLen_min, " min scans")
+        else:
+            print("Rolling ", window_size, "scans together")
+
+        window_size = int(window_size)
+        #Determine what run numbers already exist in target location
+        targetLocation_for_rolls = Path (master_file_destination / (str(rollLength) + "rolls/"))
+
+        if targetLocation_for_rolls.exists() == False:
+            print("Creating:", targetLocation_for_rolls)
+            targetLocation_for_rolls.mkdir(parents=True, exist_ok=True)
+
+        # ==========================================================
+        # enumerate all .tif files in this folder
+        # ==========================================================
+        tif_files = sorted(targetLocation_for_rolls.glob("*.tif"))
+
+        print(f"Found {len(tif_files)} .tif files in {targetLocation_for_rolls}")
+        pre_existing_images = []
+        # optional: print names for debug
+        for f in tif_files:
+            pre_existing_images.append(f.name)
+        
+
+
+        
+        runs_which_have_not_been_processed_yet = []
+        if len(tif_files) == 0:
+            print("No prexisting data found")
+            runs_which_have_not_been_processed_yet = run_number_range
+        else: #find the most recent run number that has bin processed
+
+            max_run = -float("inf")
+
+            for f in tif_files:
+
+                # extract all numbers in filename
+                numbers = re.findall(r"\d+", f.stem)
+
+                if not numbers:
+                    continue
+
+                # convert to ints
+                numbers = list(map(int, numbers))
+
+                # take max number in this file
+                file_max = max(numbers)
+
+                # update global max
+                if file_max > max_run:
+                    max_run = file_max
+
+            print("Largest run number found:", max_run)
+
+            #compute which preceding frames are need to complete the immediate frames after the max frame
+            runs_which_have_not_been_processed_yet = list(range(max_run-window_size, run_number_range[-1]))
+
+
+
+        #def process_runs_rolling_global(master_image_source, file_destination, run_numbers, open_beam, window_size, image_duration, duration_percentage_filter = 20):
+        newImages = process_runs_rolling_global(master_image_source, master_file_destination / (str(rollLength) + "rolls/"), runs_which_have_not_been_processed_yet, ob_path / ("June_OB_" + str(rollLength) + "min.tif" ),  window_size, scanLen_min*60, energyLabels = [560, 830] , duration_percentage_filter = 20)
+        tifPaths = []
+        for tif in pre_existing_images:
+            print(tif)
+            tifPaths.append(master_file_destination / (str(rollLength) +"rolls") / tif)
+
+        #print(ASTER_DESTINATION / (str(roll) +"rolls") / tif)
+        allImages = tifPaths + newImages
+        
+        balancedImages = []
+
+        for img_path in allImages:
+
+            arr = np.array(Image.open(img_path))
+
+            arr = np.squeeze(arr)
+
+            if arr.ndim == 2:
+                arr = np.stack([arr]*3, axis=-1)
+
+            arr = grey_balance(arr)
+
+            arr = (arr * 255).astype(np.uint8)
+
+            Image.fromarray(arr).save(img_path)
+
+            balancedImages.append(img_path)
+        
+  
+        return balancedImages
 
 
 # ==========================================================
